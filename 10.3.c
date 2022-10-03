63a5b386b22ca694cb9820a0199ab1fe7bbd5492
vtuvgbuybhi <2245103729@qq.com>
2022-10-03 23:30:02 +0800

    c++实验1代码练习

---
+++ 10.3.c
@@ -0,0 +1,87 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//1.定义一个整型变量，一个浮点型变量，一个字符型变量，在声明变量时初始化，输出变量的值。
+//#include<stdio.h>
+//int main()
+//{
+//	float x = 5.1;
+//	int y = 1;
+//	char c = 'b';
+//	printf("%f\n", x);
+//	printf("%d\n", y);
+//	printf("%c", c);
+//	return 0;
+//}
+
+//2.提示用户输入一个数字（表示米），将它转换为厘米，在屏幕上输出。
+//int main()
+//{
+//	double  x = 0;
+//	printf("请输入一个数字:");
+//	scanf("%lf", &x);
+//	printf("%lfcm", x * 100);
+//	return 0;
+//}
+
+//3.提示用户输入一个大写字母，将它转换为小写字母，在屏幕上输出。
+//int main()
+//{
+//	char x = '0';
+//	printf("请输入要转换的大写字母:");
+//	scanf("%c", &x);
+//	printf("%c", x + 32);
+//	return 0;
+//}
+
+//4.设学生常数为500，编程输出下列结果（引号也要输出）“How many students here ? ”
+//int main()
+//{
+//	printf("“how many studnts here ? ”\n");
+//	printf("500");
+//}
+
+//5.编写一个程序，用户输入一个摄氏温度，程序将其转成华氏温度并输出。提示：摄氏度，用符号℃表示，是摄氏温标(	C)\
+的单位，	摄氏温标(		C)	与华氏温标(		F)	的换算	式是：	C = (F - 32)÷1.8	，	F = 1.	8×C + 32
+//int main()
+//{
+//	float f, c;
+//	printf("输入一个摄氏温度:");
+//	scanf("%f", &f);
+//	c = (f - 32) / 1.8;
+//	printf("%.2f", c);
+//	return 0;
+//}
+
+//6.6、编写一个程序，计算身体质量指数，即BMI指数。它是国际上常用的衡量人体胖瘦程度以及是否健康的一个标准。计算公式为：BMI =体重÷身高2
+//int main()
+//{
+//	float weight;
+//	float height;
+//	float BIM;
+//	printf("BIM值计算\n");
+//	printf("请输入你的体重（kg）:");
+//	scanf("%f", &weight);
+//	printf("请输入你的身高（m）:");
+//	scanf("%f",&height);
+//	BIM = weight / (height * height);
+//	printf("你的BIM是%0.2f", BIM);
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
