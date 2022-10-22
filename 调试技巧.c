f24e8c133279b71c2c1d06eb6f4276ebcb0b693e
vtuvgbuybhi <2245103729@qq.com>
2022-10-22 23:44:34 +0800

    10.22

---
+++ 调试技巧.c
@@ -0,0 +1,156 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 100; i++)
+//	{
+//		printf("%d\n", i);
+//	}
+//	system("pause");
+//	return 0;
+//
+//}
+
+//F5--启动调试-和F9配合使用
+
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	for (i = 0; i <= 12; i++)
+//	{
+//		printf("hehehe\n");
+//		arr[i] = 0;
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//红点--F9切换断点
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 100; i++)
+//	{
+//		printf("%d ", i);
+//	}
+//	printf("\n");
+//	for (i = 0; i < 100; i++)
+//	{
+//		printf("%d ", 10-i);
+//	}
+//	return 0;
+//
+//}
+//int Add(int a, int b)
+//{
+//	int z = a + b;
+//	return z;
+//}
+//int main()
+//{
+//	printf("hehe\n");
+//	int a = 20;
+//	int b = 10;
+//	int c = Add(a, b);
+//	printf("%d", c);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	{
+//		int tmp = 0;
+//		printf("tmp=%d\n", tmp);
+//	}
+//
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int sum = 0;
+//	int n = 0;
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		int ret = 1;
+//		for (int j = 1; j <= i; j++)
+//		{
+//			ret *= j;
+//		}
+//		sum += ret;
+//	}
+//	printf("sum=%d", sum);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	for (i = 0; i <= 12; i++)
+//	{
+//		printf("hehehe\n");
+//		arr[i] = 0;
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	for (i = 0; i <= 12; i++)
+//	{
+//		printf("hehehe\n");
+//		arr[i] = 0;
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
