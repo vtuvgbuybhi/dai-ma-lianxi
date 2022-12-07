f0dd977791862df6382f805143c2d8b66060c7e8
vtuvgbuybhi <2245103729@qq.com>
2022-12-07 10:57:36 +0800

    12.7

---
+++ 实验6.c
@@ -0,0 +1,100 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//#define MAX 455
+//int main()
+//{
+//	char a[MAX]={0};
+//	printf("输入字符串: ");
+//	scanf("%s", &a);
+//	char* pa = a;
+//	int m = 0;
+//	int n = 0;
+//	while (*pa != '\0')
+//	{
+//		if (*pa >= 48 && *pa <= 57)
+//		{
+//			m++;
+//			pa++;
+//		}
+//		else
+//		{
+//			n++;
+//			pa++;
+//		}
+//	}
+//	printf("字符串中字符和数字的个数分别是: \n");
+//	printf("%d , %d ", n, m);
+//	return 0;
+//}
+
+
+//#include<string.h>
+//int main()
+//{
+//	char a[50] = { 0 };
+//	printf("输入字符串: ");
+//	scanf("%s", &a);
+//	int len = strlen(a);
+//	char a1[50] = { 0 };
+//	strcpy(a1, a);
+//	char* p = a;
+//	char* p1 = a1;
+//	int m = 0;
+//	for (int i = len; i >=0; i--)
+//	{
+//		if (*p == *(p1+i))
+//		{
+//			p++;
+//			m++;
+//		}
+//	}
+//	if (m == len)
+//	{
+//		printf("字符串是回文");
+//	}
+//	else
+//	{
+//		printf("no");
+//	}
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
